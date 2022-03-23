bfb8455d2970934fc7ffe4cc2fe4c32d8c786eb4
TxThinking <cloud@txthinking.com>
2022-03-23 14:39:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ mkdir _
 deno compile -A -r --unstable --target x86_64-unknown-linux-gnu -o _/testbrook_linux_amd64 https://raw.githubusercontent.com/txthinking/testbrook/master/testbrook.js
 deno compile -A -r --unstable --target x86_64-apple-darwin -o _/testbrook_darwin_amd64 https://raw.githubusercontent.com/txthinking/testbrook/master/testbrook.js
 deno compile -A -r --unstable --target aarch64-apple-darwin -o _/testbrook_darwin_arm64 https://raw.githubusercontent.com/txthinking/testbrook/master/testbrook.js
-deno compile -A -r --unstable --target x86_64-pc-windows-msvc -o _/testbrook_windows_amd64 https://raw.githubusercontent.com/txthinking/testbrook/master/testbrook.js
+deno compile -A -r --unstable --target x86_64-pc-windows-msvc -o _/testbrook_windows_amd64.exe https://raw.githubusercontent.com/txthinking/testbrook/master/testbrook.js
 
 nami release github.com/txthinking/testbrook $1 _
 
